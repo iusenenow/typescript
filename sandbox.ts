69981bb4e96ee10272fa1a2ef9a136a5133f4e38
Wei Zhang <iusenenow@gmail.com>
2020-09-25 10:28:51 +1000

    seconde update

--- sandbox.ts
+++ sandbox.ts
@@ -1,9 +1,90 @@
-const character = 'luigi'
+// Tutorial 2 - Compiling
 
-console.log(character)
+// const character = 'luigi'
+// console.log(character)
+// const inputs = document.querySelectorAll('input')
+// console.log(inputs)
+// inputs.forEach(input => console.log(input))
 
-const inputs = document.querySelectorAll('input')
+// Tutorial 3 - Type Basics
+// let character = 'mario'
+// let age = 30
+// let isBlackBelt = false
 
-console.log(inputs)
+// character = 20 character has to be a number
+// character = 'luigi'
 
-inputs.forEach(input => console.log(input))
+// age = 'yoshi' age has to be a string
+// age = 40
+
+// isBlackBelt = 'yes' isBlackBelt has to be a Boolean
+// isBlackBelt = true
+
+// const circle = (diameter: number) => {
+//   console.log(diameter * Math.PI)
+// }
+// diameter: 直径
+// circle(5)
+
+// Tutorial 4 - Object & Array Types
+// Arrays
+// let names = ['luigi', 'mario', 'yoshi']
+
+// names.push('toad')
+// names.push(3)
+// names[0] = true
+
+// let numbers = [10, 20, 30, 40]
+
+// numbers.push(25)
+// numbers.push('shaun')
+// numbers[0] = 'shaun'
+
+// let mixed = ['ken', 4, 'chun-li', 8, true]
+
+// mixed.push('ryu')
+// mixed.push(89)
+// mixed.push(false)
+
+// Objects
+// let ninja = {
+//   name: 'mario',
+//   belt: 'black',
+//   age: 30
+// }
+
+// ninja.age = 40
+// ninja.name = 'ryu'
+// ninja.belt = 30
+// ninja.skills = ['fighting', 'sneaking']
+
+// ninja = {
+//   name: 'yoshi',
+//   belt: 'orange',
+//   age: 99,
+//   // skills: []
+// }
+
+// Turorial 5 - Explicit Types
+// explicit types
+let character: string
+let age: number
+let isLoggedIn: boolean
+
+// age = 'luigi'
+age = 30
+
+// isLoggedIn = 25
+isLoggedIn = true
+
+// arrays
+let ninjas: string[] = []
+ninjas.push('shaun')
+
+// union types
+let mixed: (string | number | boolean)[] = []
+mixed.push('hello')
+mixed.push(20)
+mixed.push(false)
+
+// objects
